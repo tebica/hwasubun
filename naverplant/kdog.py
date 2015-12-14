ac6f31fcf856230a6147d1eaabe7cbbbf0ca4dc3
Park Minwoo <tebica@gmail.com>
2015-12-15 00:51:51 +0900

    first commit

---
+++ naverplant/kdog.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+open('sex.txt')
+text = open('sex.txt').read()
+ text
+text
+text.encode('utf8')
+text
+import json
+json.loads(text)
+d = json.loads(text)
+d
+d.keys()
+d[-1]
+d[-1].keys()
+d[-1][name]
+d[-1]['name']
+print d[-1]['name']
+print d[-1]['name'][0]
+print d[-1]['name'].encode('utf8')
+print d[-1]['name'][0].encode('utf8')
+print repr(d[-1]['name'][0].encode('utf8'))
+print repr(d[-1]['name'][0])
+d[0]
+d[1]
+d[0]
+d[1]
+d[0]
+get_ipython().magic(u'pwd ')
+get_ipython().magic(u'save')
+get_ipython().magic(u'save kdog')
+get_ipython().magic(u'store')
